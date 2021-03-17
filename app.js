e029dc92e338a6eef0e178324ca488657ab3df01
Rahul Khatav <rahulskhatav@gmail.com>
2021-03-18 00:12:58 +0400

    Complete App 1.0

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const { reduce } = require('async')
 const express = require(`express`)
 const request = require('request')
+const dotenv = require('dotenv')
 const app = express()
+dotenv.config()
 
 //Middleware
 //This tells which template engine to use
@@ -14,7 +16,7 @@ app.get('/', (req, res) =>{
 
 app.get('/result', (req, res) =>{
     //res.send(`You searched for ${req.query.movieName}`)
-    const url = `http://www.omdbapi.com/?apikey=6b526811&s=${req.query.movieName}`
+    const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.query.movieName}`
     request(url, function(error, response, body){
         if(!error && response.statusCode === 200){
             const data = JSON.parse(body)
@@ -28,9 +30,27 @@ app.get('/result', (req, res) =>{
 
 })
 
+app.get("/specific/:id", (req, res)=>{
+    const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.id}`
+    request(url, (error, response, body)=>{
+        if(!error && response.statusCode===200){
+            const data = JSON.parse(body)
+            //res.send(data)
+            res.render("specific", {specSheet: data})
+        }
+        else{
+            res.send("Something's not right!")
+        }
+    })
+})
+
 // app.get('/student/:rollno', (req, res) =>{
 //     res.send(`You are viewing record of ${req.params.rollno}`)
 // })
+
+app.get("*", (req, res)=>{
+    res.send("404 NOT FOUND")
+})
 
 app.listen(3000, () =>{
     console.log("Server kicked in!")
